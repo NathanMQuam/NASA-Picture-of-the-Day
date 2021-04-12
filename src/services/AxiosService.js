bc55daa9a4b3e9649a0ba6aa1e5c5e1aae9465e1
Nathan Quam <75698076+NathanMQuam@users.noreply.github.com>
2021-04-12 11:50:48 -0600

    Removed api key

--- src/services/AxiosService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-export const photoApi = axios.create({
-  baseURL: 'https://api.nasa.gov/planetary/apod?api_key=7cmDNzHalNFIG0Np6kkHHDIdcrxRvVtk4dzdzgI8',
-  timeout: 5000
-})
